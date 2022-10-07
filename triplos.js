6ec31bdbb21e4c6fa2df602da247289747703012
rubisel <rubiselreyeslara.rrl@gmail.com>
2022-10-06 19:51:24 -0500

    Algunos cambios para obtener el resultado

--- triplos.js
+++ triplos.js
@@ -1,6 +1,7 @@
 document.getElementById('btn-calcular').addEventListener('click', ()=>{
-    const operadoresDerIqz = ['/', '*'];
-    const operadoresIzqDer = ['+', '-'];
+    const operadoresIzqDer = ['/', '*'];
+    const operadoresDerIzq = ['+', '-'];
+    let arrResultados = new Array();
 
     let expresion = document.getElementById('input-expresion').value.replace(/ /g, '');
     const operadoresRegEx = /[+*\-=\(\)\/]/g;
@@ -10,14 +11,16 @@ document.getElementById('btn-calcular').addEventListener('click', ()=>{
 
             let indiceMemoria = 0;
             while(expresion.includes('(')) {
-                let grupoParentesis = expresion.substring(expresion.lastIndexOf('('), expresion.indexOf(')') + 1);
+                let grupoParentesis = expresion.substring(expresion.lastIndexOf('(') + 1, expresion.indexOf(')'));
                 const ejecucionParentesis = calcularAgrupaciones(
-                    operadoresDerIqz, operadoresIzqDer, grupoParentesis, operadoresRegEx, indiceMemoria);
+                    operadoresIzqDer, operadoresDerIzq, grupoParentesis, operadoresRegEx, indiceMemoria);
                 expresion = expresion.replace(ejecucionParentesis[0], ejecucionParentesis[1]); 
                 indiceMemoria = ejecucionParentesis[2];
+                arrResultados = arrResultados.concat(ejecucionParentesis[3]);
             }
 
-            calcularAgrupaciones(operadoresDerIqz, operadoresIzqDer, expresion, operadoresRegEx);
+            arrResultados = arrResultados.concat(
+                calcularAgrupaciones(operadoresIzqDer, operadoresDerIzq, expresion, operadoresRegEx, indiceMemoria)[3]);
 
 
 
@@ -25,17 +28,15 @@ document.getElementById('btn-calcular').addEventListener('click', ()=>{
         } else if(expresion.includes('(')){
             alert('La expresión es invalida debido a que dejo un "(" sin cerrar');
         }
+
     } else {
         alert('Disculpe, dejo un espacio en blanco, o ingreso una expresión NO válida')
     }
 });
 
-function calcularAgrupaciones(operadoresDerIqz, operadoresIzqDer, expresion, operadoresRegEx, iMemoria){
+function calcularAgrupaciones(operadoresIzqDer, operadoresDerIzq, expresion, operadoresRegEx, indiceMemoria){
     // FALTA VERIFICAR SI SOLO HAY UNA VARIABLE O VALOR DENTRO DE PARENTESIS '''''''''''''''''''''''''
-    const expresionOriginal = expresion;
-    expresion = expresion.replace('(','');
-    expresion = expresion.replace(')','');
-
+    let arrResultado = new Array();
     let arregloVariables = expresion.split(operadoresRegEx); // Obtengo las vars (a, b, c)
     let arregloOperadores = expresion.match(operadoresRegEx).concat(['']); // Obtengo los ops (+, -, ...)
     let arregloVarsOps = new Array();
@@ -45,31 +46,37 @@ function calcularAgrupaciones(operadoresDerIqz, operadoresIzqDer, expresion, ope
         arregloVarsOps.push(arregloOperadores[i]);
     }
 
-    operadoresDerIqz.forEach((operador) => {
-        while(arregloVarsOps.includes(operador)) {
-            iMemoria++;
-            let indice = arregloVarsOps.indexOf(operador);
-            arregloVarsOps[indice - 1] = '';
-            arregloVarsOps[indice] = `[${iMemoria}]`;
-            arregloVarsOps[indice + 1] = '';
-            arregloVarsOps = arregloVarsOps.filter(elemento => String(elemento).trim());
-            console.log(arregloVarsOps);
-        }
-    });
-
     operadoresIzqDer.forEach((operador) => {
-        while(arregloVarsOps.includes(operador)){
-            iMemoria++;
-            let indice = arregloVarsOps.lastIndexOf(operador);
+        while(arregloVarsOps.includes(operador)) {
+            indiceMemoria++;
+            let indice = arregloVarsOps.indexOf(operador);
+            arrResultado.push(
+                [arregloVarsOps[indice - 1], arregloVarsOps[indice], arregloVarsOps[indice + 1], `[${indiceMemoria}]`]);
+
             arregloVarsOps[indice - 1] = '';
-            arregloVarsOps[indice] = `[${iMemoria}]`;
+            arregloVarsOps[indice] = `[${indiceMemoria}]`;
             arregloVarsOps[indice + 1] = '';
             arregloVarsOps = arregloVarsOps.filter(elemento => String(elemento).trim());
             console.log(arregloVarsOps);
         }
     });
 
-    return [expresionOriginal, ...arregloVarsOps, iMemoria];
+    operadoresDerIzq.forEach((operador) => {
+        while(arregloVarsOps.includes(operador)){
+            indiceMemoria++;
+            let indice = arregloVarsOps.lastIndexOf(operador);
+            arrResultado.push(
+                [arregloVarsOps[indice - 1], arregloVarsOps[indice], arregloVarsOps[indice + 1]], `[${indiceMemoria}]`);
+
+            arregloVarsOps[indice - 1] = '';
+            arregloVarsOps[indice] = `[${indiceMemoria}]`;
+            arregloVarsOps[indice + 1] = '';
+            arregloVarsOps = arregloVarsOps.filter(elemento => String(elemento).trim());
+            console.log(arregloVarsOps);
+        }
+    });
+
+    return [`(${expresion})`, ...arregloVarsOps, indiceMemoria, arrResultado];
 
 
 
